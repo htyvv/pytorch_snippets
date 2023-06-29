7352fbb6c657efe4d1d4b7e65310ed1a7c95a911
Taeyeong Heo <htyvv@naver.com>
2023-06-29 18:08:15 +0900

    Add encoder

--- src/model/transformer/Transformer.py
+++ src/model/transformer/Transformer.py
@@ -1,5 +1,5 @@
 import copy
-from pandas import MultiIndex
+import math
 
 import torch
 from torch import nn
@@ -21,38 +21,71 @@ class Transformer(nn.Module):
         out = self.decoder(z, c)
         return out
     
-    def forward(self, x, z):
-        c = self.encode(x)
-        y = self.decode(z, c)
-        return y
+    def forward(self, src, tgt, src_mask):
+        context = self.encode(src, src_mask)
+        out = self.decode(tgt, context)
+        return out
     
+    def make_pad_mask(self, raw_query, raw_key, pad_idx=1):
+        # query: (n_batch, query_seq_len)
+        # key: (n_batch, key_seq_len)
+        query_seq_len, key_seq_len = raw_query.size(1), raw_key.size(1)
+
+        key_mask = raw_key.ne(pad_idx).unsqueeze(1).unsqueeze(2)  # (n_batch, 1, 1, key_seq_len)
+        key_mask = key_mask.repeat(1, 1, query_seq_len, 1)    # (n_batch, 1, query_seq_len, key_seq_len)
+
+        query_mask = raw_query.ne(pad_idx).unsqueeze(1).unsqueeze(3)  # (n_batch, 1, query_seq_len, 1)
+        query_mask = query_mask.repeat(1, 1, 1, key_seq_len)  # (n_batch, 1, query_seq_len, key_seq_len)
+
+        mask = key_mask & query_mask
+        mask.requires_grad = False
+        return mask
+
+    def make_src_mask(self, src):
+        # This function is Specified on "Self-Attention" (not "Cross-Attention")
+        pad_mask = self.make_pad_mask(src, src)
+        return pad_mask
+        
     
 class Encoder(nn.Module):
     
-    def __init__(self, encoder_block, n_layer):
+    def __init__(self, encoder_layer, n_layer):
         super(Encoder, self).__init__()
         self.layers = []
-        for i in range(n_layer):
-            self.layers.append(copy.deepcopy(encoder_block))
+        for _ in range(n_layer):
+            self.layers.append(copy.deepcopy(encoder_layer))
     
-    def forward(self, x):
-        out = x
+    def forward(self, src, src_mask):
+        out = src
         for layer in self.layers:
-            out = layer(out)
+            out = layer(out, src_mask)
         return out
-    
+
+
+class ResidualConnectionLayer(nn.Module):
+
+    def __init__(self):
+        super(ResidualConnectionLayer, self).__init__()
+
+    def forward(self, x, sub_layer):
+        out = x
+        out = sub_layer(out)
+        out = out + x
+        return out
+
     
 class EncoderBlock(nn.Module):
-    
+
     def __init__(self, self_attention, position_ff):
         super(EncoderBlock, self).__init__()
         self.self_attention = self_attention
         self.position_ff = position_ff
-        
-    def forward(self, x):
-        out = x
-        out = self.self_attention(out)
-        out = self.position_ff(out)
+        self.residuals = [ResidualConnectionLayer() for _ in range(2)]
+
+    def forward(self, src, src_mask):
+        out = src
+        out = self.residuals[0](out, lambda out: self.self_attention(query=out, key=out, value=out, mask=src_mask))
+        out = self.residuals[1](out, self.position_ff)
         return out
     
     
@@ -81,7 +114,7 @@ class MultiHeadAttentionLayer(nn.Module):
         key = transform(key, self.k_fc)     # (n_batch, h, seq_len, d_k)
         value = transform(value, self.v_fc) # (n_batch, h, seq_len, d_k)
 
-        out = self.calculate_self_attention(query, key, value, pad_mask) # (n_batch, h, seq_len, d_k)
+        out = self.calculate_attention(query, key, value, pad_mask) # (n_batch, h, seq_len, d_k)
         out = out.transpose(1, 2) # (n_batch, seq_len, h, d_k)
         out = out.contiguous().view(n_batch, -1, self.d_model) # (n_batch, seq_len, d_model)
         out = self.out_fc(out) # (n_batch, seq_len, d_embed)
@@ -90,13 +123,30 @@ class MultiHeadAttentionLayer(nn.Module):
     # Transformer의 input은 mini-batch 형태
     # Encoder의 input shape : (n_batch, seq_len, dim_embed)
     # Q, K, V의 shape : (n_batch, seq_len, dim_key), dim_key의 경우 QKV를 만드는 FC-layer에 의해서 결정 됨
-    def calculate_self_attention(self, query, key, value, pad_mask):
-        # query, key, value: (n_batch, seq_len, d_k)
-        # pad_mask: (n_batch, seq_len, seq_len)
-        dim_key = key.shape[-1]
-        attention_score = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(dim_key)
+    def calculate_attention(self, query, key, value, pad_mask):
+        # query, key, value: (n_batch, h, seq_len, d_k)
+        # mask: (n_batch, 1, seq_len, seq_len)
+        d_k = key.shape[-1]
+        attention_score = torch.matmul(query, key.transpose(-2, -1)) # Q x K^T, (n_batch, h, seq_len, seq_len)
+        attention_score = attention_score / math.sqrt(d_k)
         if pad_mask is not None:
             attention_score = attention_score.masked_fill(pad_mask==0, -1e9)
-        attention_prob = F.soft_max(attention_score, dim=-1)
-        out = torch.matmul(attention_prob, value)
+        attention_prob = F.softmax(attention_score, dim=-1) # (n_batch, h, seq_len, seq_len)
+        out = torch.matmul(attention_prob, value) # (n_batch, h, seq_len, d_k)
+        return out
+    
+    
+class PositionWiseFeedForwardLayer(nn.Module):
+
+    def __init__(self, fc1, fc2):
+        super(PositionWiseFeedForwardLayer, self).__init__()
+        self.fc1 = fc1   # (d_embed, d_ff)
+        self.relu = nn.ReLU()
+        self.fc2 = fc2 # (d_ff, d_embed)
+
+    def forward(self, x):
+        out = x
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
         return out
